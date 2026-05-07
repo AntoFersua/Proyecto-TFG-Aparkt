563c9cf0911578e184e7052d55cea9e0831d5e37
Nooeemii9 <noemiruiztoro.971@gmail.com>
2026-05-07 19:59:54 +0200

    comentarios de login .css .js .html

--- app/views/login/login.js
+++ app/views/login/login.js
@@ -1,20 +1,27 @@
+//Autenticación
 import { iniciarAuth, obtenerUsuario, cerrarSesion } from '../auth.js';
+//Web Components usados en la vista del login
 import "../components/header.js";
 import "../components/PerfilUsuario.js";
 import "../components/Modalpuntos.js";
 import '../components/Footer.js';
 
+//Variable que guarda al usuario (en caso de que exista sesión)
 let usuarioActual = null;
 
+//Se ejecuta cuando el DOM está completamente cargado
 document.addEventListener("DOMContentLoaded", async function () {
   console.log("Inicializando Login");
 
+  //Inicializa el sistema de autenticación
   await iniciarAuth({
+    //Lo que pasa si esta logueado
     alLoguearse: (usuario) => {
       usuarioActual = usuario;
       configurarUIUsuarioLogueado();
       inicializarFormulario();
     },
+    //Usuario no logueado
     alNoLoguearse: () => {
       configurarUIUsuarioNoLogueado();
       inicializarFormulario();
@@ -22,6 +29,7 @@ document.addEventListener("DOMContentLoaded", async function () {
   });
 });
 
+//Función para cuando el usuario está LOGUEADO
 function configurarUIUsuarioLogueado() {
   const botonPerfil = document.getElementById('perfilUsuario');
   const banner = document.getElementById('bannerUsuario');
@@ -33,6 +41,7 @@ function configurarUIUsuarioLogueado() {
   }
 }
 
+//Función cuando el usuario NO ESTÁ LOGUEADO
 function configurarUIUsuarioNoLogueado() {
   const botonPerfil = document.getElementById('perfilUsuario');
   if (botonPerfil) {
@@ -42,14 +51,17 @@ function configurarUIUsuarioNoLogueado() {
   }
 }
 
+//Funcion para validar el formulario
 function inicializarFormulario() {
   // Inicializar JustValidate
   const validador = new JustValidate("#loginForm", {
+    //Validamos antes de enviar
     validateBeforeSubmitting: true,
+    //Enfocamos al primer campo con error
     focusInvalidField: true,
   });
 
-  // EMAIL
+  // Validación EMAIL. Es obligatorio
   validador.addField("#email", [
     {
       rule: "required",
@@ -59,7 +71,7 @@ function inicializarFormulario() {
     errorsContainer: "#error-email",
   });
 
-  // PASSWORD
+  // Validación CONTRASEÑA
   validador.addField("#password", [
     {
       rule: "required",
@@ -69,15 +81,17 @@ function inicializarFormulario() {
     errorsContainer: "#error-password",
   });
 
-  // SUBMIT
+  // Cuando el formulario es válido
   validador.onSuccess((event) => {
     event.preventDefault();
 
+    //Recogemos los datos del formulario
     const datos = {
       usuario: document.querySelector("#email").value.trim(),
       contrasena: document.querySelector("#password").value.trim(),
     };
 
+    //Petición mediante Fetch al Backedn PHP para login
     fetch("../../controllers/LoginController.php", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
@@ -85,9 +99,12 @@ function inicializarFormulario() {
     })
       .then((response) => response.json())
       .then((data) => {
+        //Si todo es correcto
         if (data.status === "ok") {
           alert(data.mensaje);
+          //redirigimos a la app principal aparkt
           window.location.href = '../aparkt/aparkt.html';
+        //Si el login es incorrrecto
         } else {
           alert("Error en el login");
         }
