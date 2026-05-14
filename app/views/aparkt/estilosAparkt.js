b81689ed68c319a3ebc617d73a1d3cc4ebce29f3
Celia <cml0034@alu.medac.es>
2026-05-14 18:38:58 +0200

    estilos mobile

--- app/views/aparkt/estilosAparkt.js
+++ app/views/aparkt/estilosAparkt.js
@@ -1,4 +1,5 @@
-(() => {
+document.addEventListener("DOMContentLoaded", () => {
+  // Carrusel
   const carrusel = $(".owl-carousel");
 
   carrusel.owlCarousel({
@@ -7,64 +8,60 @@
     margin: 0,
     nav: true,
     dots: true,
-
-    // Mejora UX
     smartSpeed: 600,
     autoplay: true,
     autoplayTimeout: 8000,
     autoplayHoverPause: true,
-
-    // Interacción
     mouseDrag: true,
     touchDrag: true,
-
-    // Flechas personalizadas
     navText: [
       "<span class='arrow left'>‹</span>",
       "<span class='arrow right'>›</span>",
     ],
-
-    // Responsive por si luego quieres más slides
     responsive: {
-      768: {
-        items: 1,
-      },
-      1024: {
-        items: 1,
-      },
+      768: { items: 1 },
+      1024: { items: 1 },
     },
   });
-})();
 
-// Animaciones con GSAP
-gsap.registerPlugin(ScrollTrigger);
-const horizontalSection = document.querySelector(".horizontal");
-gsap.to(horizontalSection, {
-  x: () =>
-    -(horizontalSection.scrollWidth - document.documentElement.clientWidth) +
-    "px",
-  ease: "none",
-  scrollTrigger: {
-    trigger: horizontalSection,
-    start: "top top",
-    end: () => "+=" + (horizontalSection.scrollWidth - innerWidth),
-    scrub: true,
-    pin: true,
-    anticipatePin: 1,
-  },
-});
+  // GSAP Scroll Horizontal - solo desktop con matchMedia
+  gsap.registerPlugin(ScrollTrigger);
 
-// Coche que avanza con el scroll horizontal
-const car = document.querySelector(".car");
-gsap.to(car, {
-  x: () => horizontalSection.scrollWidth - 200,
-  ease: "none",
-  scrollTrigger: {
-    trigger: horizontalSection,
-    start: "top top",
-    end: () => "+=" + (horizontalSection.scrollWidth - innerWidth),
-    scrub: 0.5,
-  },
+  const mm = gsap.matchMedia();
+
+  mm.add("(min-width: 769px)", () => {
+    const horizontalSection = document.querySelector(".horizontal");
+    if (horizontalSection) {
+      gsap.to(horizontalSection, {
+        x: () =>
+          -(horizontalSection.scrollWidth - document.documentElement.clientWidth) +
+          "px",
+        ease: "none",
+        scrollTrigger: {
+          trigger: horizontalSection,
+          start: "top top",
+          end: () => "+=" + (horizontalSection.scrollWidth - innerWidth),
+          scrub: true,
+          pin: true,
+          anticipatePin: 1,
+        },
+      });
+
+      const car = document.querySelector(".car");
+      if (car) {
+        gsap.to(car, {
+          x: () => horizontalSection.scrollWidth - 200,
+          ease: "none",
+          scrollTrigger: {
+            trigger: horizontalSection,
+            start: "top top",
+            end: () => "+=" + (horizontalSection.scrollWidth - innerWidth),
+            scrub: 0.5,
+          },
+        });
+      }
+    }
+  });
 });
 
 
